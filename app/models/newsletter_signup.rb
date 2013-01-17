793fa4aeaa74a6e043e1f172767e2f2c6c316c4a
Jay Zeschin <jay.zeschin@modeset.com>
2013-01-16 21:47:57 -0700

    Clean up subscription code

--- app/models/newsletter_signup.rb
+++ app/models/newsletter_signup.rb
@@ -3,8 +3,7 @@ class NewsletterSignup < ActiveRecord::Base
   after_create :subscribe_to_mailchimp
 
   def subscribe_to_mailchimp(list_id = ENV['NEWSLETTER_LIST_ID'])
-    gb = Gibbon.new
-    Rails.logger.info gb.list_subscribe({:id => list_id, :email_address => self.email, :merge_vars => {:FNAME => self.first_name, :LNAME => self.last_name}})
+    Gibbon.new.list_subscribe({:id => list_id, :email_address => self.email, :merge_vars => {:FNAME => self.first_name, :LNAME => self.last_name}})
   end
 
 end
