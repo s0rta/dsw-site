2537ecdb7b46e163f7941b8e5394c5c9b2bbc27f
Jay Zeschin <jay.zeschin@modeset.com>
2013-01-16 21:30:03 -0700

    Auto-submit signups via Mailchimp

--- app/models/newsletter_signup.rb
+++ app/models/newsletter_signup.rb
@@ -1,3 +1,10 @@
 class NewsletterSignup < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+  after_create :subscribe_to_mailchimp
+
+  def subscribe_to_mailchimp(list_id = ENV['NEWSLETTER_LIST_ID'])
+    gb = Gibbon.new
+    Rails.logger.info gb.list_subscribe({:id => list_id, :email_address => self.email, :merge_vars => {:FNAME => self.first_name, :LNAME => self.last_name}})
+  end
+
 end
