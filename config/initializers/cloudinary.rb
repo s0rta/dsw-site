10282de39cb656058b6af7a0cf9c0077d300ef15
Jay Zeschin <jay.zeschin@modeset.com>
2013-01-16 19:32:38 -0700

    Try the cloudinary shim a different way

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,4 @@
 # Shim in Cloudinary support for the Cmsimple image uploader
-require 'cmsimple'
-require 'cloudinary'
-Cmsimple::ImageAttachmentUploader.send :include, Cloudinary::CarrierWave
+Rails.application.config.after_initialize do
+  ImageAttachmentUploader.send :include, Cloudinary::CarrierWave
+end
