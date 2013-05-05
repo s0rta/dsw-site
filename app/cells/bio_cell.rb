40ebefeb2ede60e31948b669d68750303688811c
Jay Zeschin <jay.zeschin@modeset.com>
2013-05-05 12:52:02 -0600

    Allow bio images to be edited via the CMS.

--- app/cells/bio_cell.rb
+++ app/cells/bio_cell.rb
@@ -1,10 +1,11 @@
 class BioCell < Cell::Rails
 
   def display(snippet)
-    @full_name = snippet.full_name
-    @company = snippet.company
-    @title = snippet.title
-    @copy = snippet.copy
+    @full_name  = snippet.full_name
+    @company    = snippet.company
+    @title      = snippet.title
+    @copy       = snippet.copy
+    @image      = snippet.image
     render view: :display
   end
 
