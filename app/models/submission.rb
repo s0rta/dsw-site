683aa4f22edf9962ab2239b9dac8ea23dbee27df
Jay Zeschin <jay.zeschin@modeset.com>
2013-06-23 23:24:15 -0600

    Submissions should use nulls instead of null-ish default columns

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,8 +5,7 @@ class Submission < ActiveRecord::Base
   FORMATS = [ 'Presentation',
               'Panel',
               'Workshop',
-              'Social event',
-              'Something else entirely' ]
+              'Social event' ]
 
   DAYS =  [ 'Monday',
             'Tuesday',
@@ -14,8 +13,7 @@ class Submission < ActiveRecord::Base
             'Thursday',
             'Friday',
             'Weekend before',
-            'Weekend after',
-            'Not sure / don\'t care ' ]
+            'Weekend after' ]
 
   TIME_RANGES = [ 'Early morning',
                   'Breakfast',
@@ -25,8 +23,7 @@ class Submission < ActiveRecord::Base
                   'Afternoon',
                   'Happy hour',
                   'Evening',
-                  'Late night',
-                  "Not sure / don't care" ]
+                  'Late night' ]
 
   belongs_to :submitter, class_name: 'User'
   belongs_to :track
@@ -35,11 +32,14 @@ class Submission < ActiveRecord::Base
   validates :description, presence: true
   validates :contact_email, presence: true
   validates :format,  presence: true,
-                      inclusion: { in: FORMATS }
+                      inclusion: { in: FORMATS,
+                                   allow_nil: true }
   validates :day,  presence: true,
-                      inclusion: { in: DAYS }
+                      inclusion: { in: DAYS,
+                                   allow_nil: true }
   validates :time_range,  presence: true,
-                      inclusion: { in: TIME_RANGES }
+                      inclusion: { in: TIME_RANGES,
+                                   allow_nil: true }
   validates :track_id, presence: true
 
   after_create :notify_track_chair
---
+++ db/migrate/20130621041352_clean_up_submission_nulls.rb
@@ -0,0 +1,10 @@
+class CleanUpSubmissionNulls < ActiveRecord::Migration
+  def up
+    Submission.where(format: 'Something else entirely').update_all(format: nil)
+    Submission.where(day: 'Not sure / don\'t care ').update_all(day: nil)
+    Submission.where(time_range: 'Not sure / don\'t care').update_all(time_range: nil)
+  end
+
+  def down
+  end
+end
