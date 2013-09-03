4e23e1128d4a9938a6b375bbf8bade5fceef7c22
Jay Zeschin <jay.zeschin@modeset.com>
2013-09-03 08:34:08 -0600

    Remove Zerista events when a panel picker event is destroyed

--- app/jobs/zerista_propagation_job.rb
+++ app/jobs/zerista_propagation_job.rb
@@ -3,7 +3,11 @@ class ZeristaPropagationJob
 
   def perform(submission_id)
     ActiveRecord::Base.connection_pool.with_connection do
-      Submission.find(submission_id).propagate_to_zerista
+      if submission = Submission.find_by_id(submission_id)
+        submission.propagate_to_zerista
+      else
+        Zerista::Client.new(ENV['ZERISTA_SUBDOMAIN'], ENV['ZERISTA_KEY_ID'], ENV['ZERISTA_SIGNING_KEY']).delete_event_by_client_id submission_id
+      end
     end
   end
 end
