2e3f726d919316214c41b13ca4c5e01803b51e6c
Jay Zeschin <jay.zeschin@modeset.com>
2013-09-07 18:51:05 -0600

    Update basecamp microsite

---
+++ public/basecamp/js/scripts.js
@@ -0,0 +1,25 @@
+/*------------------------------------*\
+    //Google Maps
+\*------------------------------------*/
+function initialize() {
+
+
+    var theLatlng = new google.maps.LatLng(39.7477986, -104.9949057);
+    var mapOptions = {
+        zoom: 17,
+        center: theLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: theLatlng,
+        map: map,
+        title:"Hello World!"
+    });
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(theLatlng);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
