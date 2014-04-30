dbd90b04570be7b1c4f91cfc6601b93316adfa21
Jay Zeschin <jay.zeschin@modeset.com>
2014-04-29 23:26:19 -0600

    Rework and update the panel picker flow for 2014
    Bulk of #27.

--- spec/features/submitting_spec.rb
+++ spec/features/submitting_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Creating a submission' do
 
-  let(:homepage) { Cmsimple::Page.create is_root: true, title: 'Home', template: 'default' }
+  let(:homepage) { Cmsimple::Page.create is_root: true, title: 'Home', template: 'landing_2014' }
 
   before do
     homepage.publish!
@@ -13,25 +13,18 @@ feature 'Creating a submission' do
   end
 
   scenario 'User submits a new idea' do
-    pending 'Temporarily disabled'
-    visit '/'
-    within '.primary-navigation' do
-      click_link 'Submit a Session'
-    end
+    visit '/panel-picker/submit'
     click_link 'Sign in with LinkedIn to continue'
     select 'Panel', from: 'submission_format'
     select 'Bizness', from: 'submission_track_id'
-    next_buttons = all(:css, 'button', text: 'Next')
-    next_buttons[0].click
+    all('button', text: 'Next', visible: true).last.click
     fill_in 'submission_title', with: 'Some talk'
     fill_in 'submission_description', with: 'I am going to give a talk.'
-    next_buttons[1].click
-    select 'Monday', from: 'submission_day'
+    all('button', text: 'Next', visible: true).last.click
     fill_in 'submission_location', with: 'City Hall'
-    select 'Evening', from: 'submission_time_range'
-    next_buttons[2].click
+    all('button', text: 'Next', visible: true).last.click
     fill_in 'submission_notes', with: 'I want to do a panel with more people.'
-    next_buttons[3].click
+    all('button', text: 'Next', visible: true).last.click
     fill_in 'submission_contact_email', with: 'test2@example.com'
     click_button 'Submit'
     expect(page).to have_content('Thanks For Submitting!')
