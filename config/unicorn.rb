c50cbe41b3d81574858060600dee53375eaed3e0
Jay Zeschin <jay.zeschin@modeset.com>
2014-06-04 16:31:09 -0600

    Update with canonical Unicorn configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout (ENV['TIMEOUT'] || 15).to_i
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+timeout (ENV['TIMEOUT'] || 10).to_i
 preload_app true
-listen ENV['PORT'].to_i || 3000, tcp_nopush: false
+listen (ENV['PORT'] || 3000).to_i, tcp_nopush: false
 
 before_fork do |server, worker|
 
@@ -15,10 +15,6 @@ before_fork do |server, worker|
     Rails.logger.info('Disconnected from ActiveRecord')
   end
 
-  if defined?(Redis)
-    Redis.current.quit
-  end
-
 end
 
 after_fork do |server, worker|
@@ -27,28 +23,18 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  # Duplicated in config/initializers/redis.rb
-  if defined?(Redis)
-    url = ENV['REDISCLOUD_URL'] || ENV['MYREDIS_URL'] || ENV['REDISTOGO_URL' || ENV['REDIS_URL']]
-    if url
-      uri = URI.parse(url)
-      Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-      Rails.logger.info('Connected to Redis')
-    end
+  if defined?(ActiveRecord::Base)
+    Rails.logger.info('Connected to ActiveRecord')
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = (ENV['DB_REAP_FREQ'] || 10).to_i # seconds
+    config['pool']            =   (ENV['DB_POOL'] || 2).to_i
+    ActiveRecord::Base.establish_connection(config)
   end
 
-  # Reset the object cache store (Dalli)
   if Rails.cache.respond_to?(:reset)
     Rails.cache.reset
     Rails.logger.info('Rails cache has been reset')
-  end
-
-  # Reconfigure the DB connection pool
-  if defined?(ActiveRecord::Base)
-    config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'].to_i || 10 # seconds
-    config['pool']              = ENV['DB_POOL'].to_i || 5
-    ActiveRecord::Base.establish_connection(config)
   end
 
 end
