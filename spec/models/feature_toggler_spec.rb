0997209e636a35a48bf9b10c13fffe598a32c247
Jay Zeschin <jay.zeschin@modeset.com>
2014-08-05 14:11:40 -0600

    Upgrade to Rspec 3 and capybara 2.4
    Fixes #49

--- spec/models/feature_toggler_spec.rb
+++ spec/models/feature_toggler_spec.rb
@@ -8,52 +8,52 @@ describe FeatureToggler do
 
   describe 'checking for the feedback feature' do
     it 'is inactive by default' do
-      expect(FeatureToggler.feedback_active?).to be_false
+      expect(FeatureToggler.feedback_active?).to be_falsy
     end
 
     it 'is active when it has been activated' do
       FeatureToggler.activate_feedback!
-      expect(FeatureToggler.feedback_active?).to be_true
+      expect(FeatureToggler.feedback_active?).to be_truthy
     end
 
     it 'is inactive when it has been deactivated' do
       FeatureToggler.activate_feedback!
       FeatureToggler.deactivate_feedback!
-      expect(FeatureToggler.feedback_active?).to be_false
+      expect(FeatureToggler.feedback_active?).to be_falsy
     end
   end
 
   describe 'checking for the submission feature' do
     it 'is inactive by default' do
-      expect(FeatureToggler.submission_active?).to be_false
+      expect(FeatureToggler.submission_active?).to be_falsy
     end
 
     it 'is active when it has been activated' do
       FeatureToggler.activate_submission!
-      expect(FeatureToggler.submission_active?).to be_true
+      expect(FeatureToggler.submission_active?).to be_truthy
     end
 
     it 'is inactive when it has been deactivated' do
       FeatureToggler.activate_submission!
       FeatureToggler.deactivate_submission!
-      expect(FeatureToggler.submission_active?).to be_false
+      expect(FeatureToggler.submission_active?).to be_falsy
     end
   end
 
   describe 'checking for the registration feature' do
     it 'is inactive by default' do
-      expect(FeatureToggler.registration_active?).to be_false
+      expect(FeatureToggler.registration_active?).to be_falsy
     end
 
     it 'is active when it has been activated' do
       FeatureToggler.activate_registration!
-      expect(FeatureToggler.registration_active?).to be_true
+      expect(FeatureToggler.registration_active?).to be_truthy
     end
 
     it 'is inactive when it has been deactivated' do
       FeatureToggler.activate_registration!
       FeatureToggler.deactivate_registration!
-      expect(FeatureToggler.registration_active?).to be_false
+      expect(FeatureToggler.registration_active?).to be_falsy
     end
   end
 
