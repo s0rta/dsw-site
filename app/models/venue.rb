b935e421904a9c7e28247cab5a5af434cfe45b47
Jay Zeschin <jay.zeschin@modeset.com>
2014-08-06 00:19:57 -0600

    Don’t include name when we send a venue to google maps

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,7 +3,7 @@ class Venue < ActiveRecord::Base
   attr_accessible :contact_email, :contact_name, :contact_phone, :description, :name, :address, :city, :state
 
   def address_for_google_maps
-    ([ name, address, city, state ] * ', ').gsub(' ','+')
+    ([ address, city, state ] * ', ').gsub(' ','+')
   end
 
 end
