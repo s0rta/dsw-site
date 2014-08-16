d16a30a812f3ba2a419287bdd249b61a89f8f3bd
Jay Zeschin <jay.zeschin@modeset.com>
2014-08-16 11:07:45 -0600

    Show the session day/date on the detail page
    Fixes #56

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,5 +1,11 @@
 module ScheduleHelper
+
   def human_time_range(submission)
     "#{submission.start_hour.hours.since(Date.today.beginning_of_day).strftime('%l:%M%P')} &mdash; #{submission.end_hour.hours.since(Date.today.beginning_of_day).strftime('%l:%M%P')}".html_safe
   end
+
+  def formatted_start_date_for_index(index, format = '%B %-d, %Y')
+    (Date.new(2014, 9, 15) + index.days).strftime(format)
+  end
+
 end
