51954ea7d33655a00bc2534d915df66e59580cd3
Jay Zeschin <jay.zeschin@modeset.com>
2014-09-12 09:51:07 -0600

    Fixup time zones for calendar

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -172,7 +172,7 @@ class Submission < ActiveRecord::Base
   end
 
   def week_start
-    Date.new(2014, 9, 15).at_beginning_of_day
+    ActiveSupport::TimeZone.new('America/Denver').local(2014, 9, 15).at_beginning_of_day
   end
 
   def start_datetime
