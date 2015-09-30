f18afea3361dce9b317483889099ae78d209ee39
Jay Zeschin <jay.zeschin@modeset.com>
2015-09-30 01:13:18 -0600

    Copy tweaks

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -81,35 +81,35 @@ class NotificationsMailer < ActionMailer::Base
     @day = 0
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
   def notify_of_tuesday_daily_schedule(registration)
     @day = 1
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
   def notify_of_wednesday_daily_schedule(registration)
     @day = 2
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
   def notify_of_thursday_daily_schedule(registration)
     @day = 3
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
   def notify_of_friday_daily_schedule(registration)
     @day = 4
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
 end
