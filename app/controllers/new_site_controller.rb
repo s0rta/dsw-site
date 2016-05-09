e88cd4e01abfa5f2ad08283db9e14719bc8e6c5a
Marla Brizel <marlabrizel@gmail.com>
2016-05-08 18:56:29 -0600

    Move new site to root

--- app/controllers/new_site_controller.rb
+++ app/controllers/new_site_controller.rb
@@ -1,5 +1,5 @@
 class NewSiteController < ApplicationController
   def index
-    render template: "new_site/#{params[:page]}"
+    render template: "new_site/#{params[:page]}", layout: 'new_site'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,8 +29,6 @@ Rails.application.routes.draw do
   resources :volunteer_signups, only: :create
   resources :sponsor_signups, only: :create
 
-  get '/new-site(/*page)', to: 'new_site#index', as: :new_site, defaults: {page: :index}
-
   get '/schedule', to: 'schedules#index', as: :schedules
   get '/my-schedule', to: 'schedules#my_schedule', as: :my_schedule
   get '/schedule/:id', to: 'schedules#show', as: :schedule
@@ -45,4 +43,5 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
+  get '/(*page)', to: 'new_site#index', as: :new_site, defaults: {page: :index}
 end
