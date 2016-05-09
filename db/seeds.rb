415c5e2523117fd70d2287494b5bdbcb397d71e6
Jay Zeschin <jay.zeschin@modeset.com>
2016-05-09 07:50:08 -0600

    Remove cmsimple from seeds as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-unless Cmsimple::Page.where(is_root: true).first
-  page = Cmsimple::Page.create is_root: true, title: 'Home', template: 'default'
-  page.publish!
-end
-
 { 'Founder'         => 'bulb',
   'Growth'          => 'graph',
   'Designer'        => 'pen',
