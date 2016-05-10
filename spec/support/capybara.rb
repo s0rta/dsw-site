ee6110e8caf88a48903eaedb3174f8c9fdfa233d
Jay Zeschin <jay.zeschin@modeset.com>
2016-05-09 21:51:05 -0600

    Force window size for CI

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,3 +2,12 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 Capybara.default_driver = :selenium
+
+RSpec.configure do |config|
+  config.prepend_before(:each, type: :feature) do
+    if Capybara.current_session.driver.browser.respond_to?(:manage)
+      Capybara.current_session.driver.browser.manage.window.resize_to(1440, 768)
+    end
+  end
+end
+
