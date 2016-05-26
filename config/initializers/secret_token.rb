3b8f0c5b2df7035a8432a4f99455f2450af3341e
Jay Zeschin <jay.zeschin@modeset.com>
2016-05-26 11:07:53 -0600

    Remove secret token since it's no longer used

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Denverstartupweek::Application.config.secret_token = 'be11af357736758581c767c9c91e12315e8d8bba2ac64e89c68e4d7a252997b393d3ba837981c64a34001b7a4100b34887eaae92e4ad4734ea6c38a6218ca516'
