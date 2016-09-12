7ca6cd35d12ec7cfa419022ef9cc00ab071d6002
Jay Zeschin <jay.zeschin@modeset.com>
2016-09-12 02:18:55 -0600

    Use the right e-mail

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -95,7 +95,7 @@ class NotificationsMailer < ActionMailer::Base
     @day = 0
     @registration = registration
     @sessions = @registration.submissions.where(start_day: @day + 2).order('start_hour ASC')
-    mail to: @registration.contact_email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
+    mail to: @registration.user.email, subject: "Your Denver Startup Week Daily Schedule for #{formatted_start_date_for_index(@day, '%A %-m/%-d')}"
   end
 
   def notify_of_tuesday_daily_schedule(registration)
