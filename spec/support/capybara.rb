5e7d1a2dced567f103b8d35cae3072274ad706a9
Jay Zeschin <jay.zeschin@modeset.com>
2016-09-11 23:52:45 -0600

    Bring back simple registration for kiosk mode

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,6 +5,7 @@ Capybara.default_driver = :selenium
 
 RSpec.configure do |config|
   config.prepend_before(:each, type: :feature) do
+    Capybara.reset_session!
     if Capybara.current_session.driver.browser.respond_to?(:manage)
       Capybara.current_session.driver.browser.manage.window.resize_to(1440, 768)
     end
