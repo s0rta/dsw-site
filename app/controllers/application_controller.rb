e3faf60a8fb40a51143c7fcfa86ac7e735ca6b6c
Jay Zeschin <jay.zeschin@modeset.com>
2017-05-07 20:26:55 -0600

    Make sure paper_trail tracks the right user for changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require "application_responder"
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
 
   self.responder = ApplicationResponder
   respond_to :html
