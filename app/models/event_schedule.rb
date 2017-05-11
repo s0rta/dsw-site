5f741df6c3bdbbf4ec58e39477f00d4e19b22550
Jay Zeschin <jay.zeschin@modeset.com>
2017-05-10 20:23:49 -0600

    Switch voting to use the schedule-based toggler

--- app/models/event_schedule.rb
+++ app/models/event_schedule.rb
@@ -2,8 +2,8 @@ module EventSchedule
   SUBMISSION_OPEN_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-03-19').freeze
   SUBMISSION_CLOSE_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-04-21').freeze
 
-  VOTING_OPEN_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-05-01').freeze
-  VOTING_CLOSE_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-05-22').freeze
+  VOTING_OPEN_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-05-10').freeze
+  VOTING_CLOSE_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-05-29').freeze
 
   REGISTRATION_OPEN_DATE = ActiveSupport::TimeZone['America/Denver'].parse('2017-06-26').freeze
 
@@ -14,6 +14,11 @@ module EventSchedule
     def submissions_open?
       Time.zone.now > SUBMISSION_OPEN_DATE.at_beginning_of_day &&
         Time.zone.now < SUBMISSION_CLOSE_DATE.at_end_of_day
+    end
+
+    def feedback_open?
+      Time.zone.now > VOTING_OPEN_DATE.at_beginning_of_day &&
+        Time.zone.now < VOTING_CLOSE_DATE.at_end_of_day
     end
 
     def week_started?
--- spec/models/feature_toggler_spec.rb
+++ spec/models/feature_toggler_spec.rb
@@ -6,23 +6,6 @@ describe FeatureToggler do
     FeatureToggler.clear
   end
 
-  describe 'checking for the feedback feature' do
-    it 'is inactive by default' do
-      expect(FeatureToggler.feedback_active?).to be_falsy
-    end
-
-    it 'is active when it has been activated' do
-      FeatureToggler.activate_feedback!
-      expect(FeatureToggler.feedback_active?).to be_truthy
-    end
-
-    it 'is inactive when it has been deactivated' do
-      FeatureToggler.activate_feedback!
-      FeatureToggler.deactivate_feedback!
-      expect(FeatureToggler.feedback_active?).to be_falsy
-    end
-  end
-
   describe 'checking for the registration feature' do
     it 'is inactive by default' do
       expect(FeatureToggler.registration_active?).to be_falsy
