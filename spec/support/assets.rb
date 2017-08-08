2fe2ac420570efb82862410861cebaf98b9afb08
Jay Zeschin <jay.zeschin@modeset.com>
2017-08-07 21:46:22 -0600

    Precompile assets to avoid Capybara feature timeouts

---
+++ spec/support/assets.rb
@@ -0,0 +1,12 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    # Precompile assets so our first test doesn't time out
+    # http://code.dblock.org/2013/08/04/precompiling-rails-assets-before-rspeccapybara-integration-tests.html
+    message = 'Precompiling assets to avoid Capybara timeouts'
+    ms = Benchmark.ms do
+      assets = Rails.application.config.assets.precompile
+      ActionView::Base.assets_manifest.compile(assets)
+    end
+    puts '%s (%.1fms)' % [ message, ms ]
+  end
+end
