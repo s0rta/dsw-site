ced93aa7657192922eb3d102eb57832eeddd7103
Jay Zeschin <jay.zeschin@modeset.com>
2017-09-05 22:51:33 -0600

    Modify sponsorship migration to work in production

--- db/migrate/20170906024523_populate_existing_sponsor_logos_from_assets.rb
+++ db/migrate/20170906024523_populate_existing_sponsor_logos_from_assets.rb
@@ -3,8 +3,13 @@ class PopulateExistingSponsorLogosFromAssets < ActiveRecord::Migration[5.1]
     Sponsorship.reset_column_information
     Sponsorship.find_each do |s|
       logo_asset = s.attributes['logo']
+      logo_asset_path = "redesign/sponsors/#{logo_asset}"
       next unless logo_asset.present?
-      path = Rails.application.assets.find_asset("redesign/sponsors/#{logo_asset}").filename
+      path = if Rails.application.assets
+        Rails.application.assets.find_asset(logo_asset_path).filename
+      else
+        "public/assets/#{Rails.application.assets_manifest.assets[logo_asset_path]}"
+      end
       Rails.logger.info "Setting logo for #{s.name} to #{logo_asset} from #{path}"
       s.logo = File.open(path)
       s.save(validate: false)
