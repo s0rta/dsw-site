1790d035b04eb8c3e76505c01b6e944d47193243
Jay Zeschin <jay.zeschin@modeset.com>
2017-09-06 09:59:42 -0600

    Fix specs

--- spec/features/content_pages_spec.rb
+++ spec/features/content_pages_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 feature 'Content-only pages' do
   scenario 'the sponsors page' do
     visit '/'
-    find('#menu-icon').click
     click_link 'Sponsors'
-    expect(page).to have_content("OUR #{Date.today.year} SPONSORS")
+    expect(page).to have_content("Our #{Date.today.year} Sponsors")
   end
 end
