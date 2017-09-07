9ba092af33159371cc1d079c79cfe62d26ec66a2
Jay Zeschin <jay.zeschin@modeset.com>
2017-09-07 11:27:06 -0600

    Make submission states dropdown nicer

--- app/admin/submissions.rb
+++ app/admin/submissions.rb
@@ -139,7 +139,7 @@ ActiveAdmin.register Submission do
       f.input :year
       f.input :submitter_id, as: :ajax_select, data: { url: filter_admin_users_path, search_fields: [ :name, :email ] }
       f.input :track_id, as: :select, collection: Track.all.map {|t| [ t.name, t.id ]}, include_blank: false
-      f.input :state, as: :select, collection: Submission.states, include_blank: false
+      f.input :state, as: :select, collection: Submission.states.map { |s| [ s.name.titleize, s.name ] }, include_blank: false
       f.input :format, as: :select, collection: Submission::FORMATS, include_blank: true
       f.input :time_range, as: :select, label: 'Submitted Time Range', collection: Submission::TIME_RANGES, include_blank: true, input_html: { disabled: true }
       f.input :start_day, as: :select, collection: Submission::DAYS.invert, include_blank: true
