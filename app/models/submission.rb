fe626a59aaa6660845c262b6abba0b1d8089842c
Jay Zeschin <jay.zeschin@modeset.com>
2017-09-17 15:02:07 -0600

    Revert "Fix join/include clobbering on by_day endpoint"
    This reverts commit 60f56005b214815bb4603f706ede971f46104fae.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -118,9 +118,8 @@ class Submission < ApplicationRecord
       joins(:user_registrations).
         where(registrations: { user_id: user.id })
     elsif filter.present?
-      # I don't think this will work if we want to filter on both track & cluster
-      # Fine for now, but something to be aware of in the future
-      joins(:track, :cluster).
+      joins(:track).
+        joins('LEFT OUTER JOIN clusters ON submissions.cluster_id = clusters.id').
         where('LOWER(clusters.name) = LOWER(:name) OR LOWER(tracks.name) = LOWER(:name)', name: filter)
     else
       all
@@ -131,6 +130,7 @@ class Submission < ApplicationRecord
     includes(:venue,
              :submitter,
              :track,
+             :cluster,
              sponsorship: :track)
   end
 
