f1be1c104840054a68576877ac3e345ba607c722
Jay Zeschin <jay.zeschin@modeset.com>
2017-09-27 07:46:41 -0600

    Update Wednesday e-mail for 2017

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -2,27 +2,27 @@ class MailPreview < ActionMailer::Preview
 
   def notify_of_monday_daily_schedule
     registration = Registration.find(5)
-    NotificationsMailer.notify_of_monday_daily_schedule(registration)
+    DailyScheduleMailer.notify_of_monday_daily_schedule(registration)
   end
 
   def notify_of_tuesday_daily_schedule
     registration = Registration.find(5)
-    NotificationsMailer.notify_of_tuesday_daily_schedule(registration)
+    DailyScheduleMailer.notify_of_tuesday_daily_schedule(registration)
   end
 
   def notify_of_wednesday_daily_schedule
     registration = Registration.find(5)
-    NotificationsMailer.notify_of_wednesday_daily_schedule(registration)
+    DailyScheduleMailer.notify_of_wednesday_daily_schedule(registration)
   end
 
   def notify_of_thursday_daily_schedule
     registration = Registration.find(5)
-    NotificationsMailer.notify_of_thursday_daily_schedule(registration)
+    DailyScheduleMailer.notify_of_thursday_daily_schedule(registration)
   end
 
   def notify_of_friday_daily_schedule
     registration = Registration.find(5)
-    NotificationsMailer.notify_of_friday_daily_schedule(registration)
+    DailyScheduleMailer.notify_of_friday_daily_schedule(registration)
   end
 
 end
