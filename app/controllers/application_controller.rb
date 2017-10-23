09508726fab955a08103d6dae1d791786180dd4f
Jay Zeschin <jay.zeschin@modeset.com>
2017-10-23 14:46:36 -0600

    Users can edit profiles (name, e-mail, password)
    Only tested the happy path scenarios for the moment.
    
    Fixes #205

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(email remember_me name password password_confirmation))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name))
   end
 
   def registered?
---
+++ spec/features/editing_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature 'Editing my profile' do
+
+  before do
+    allow(ListSubscriptionJob).to receive(:perform_async)
+  end
+
+  scenario 'Creating an editing an account from the dashboard' do
+    visit '/dashboard'
+    click_link 'Create an account'
+    fill_in 'Name', with: 'Test Registrant'
+    fill_in 'E-mail Address', with: 'test2@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm Password', with: 'password'
+    click_button 'Sign Up'
+    click_link 'My Profile'
+    fill_in 'Name', with: 'Test Registrant 2'
+    fill_in 'user_current_password', with: 'password'
+    click_button 'Update'
+    expect(page).to have_text('Test Registrant 2')
+  end
+end
