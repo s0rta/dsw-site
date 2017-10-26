39f68cabe5a9216055e80e2cf0364ce520b4131f
Jay Zeschin <jay.zeschin@modeset.com>
2017-10-26 15:47:19 -0600

    Add NR instrumentation for Icalendar gem

--- config/initializers/icalendar.rb
+++ config/initializers/icalendar.rb
@@ -1,3 +1,12 @@
 require 'tzinfo'
 require 'icalendar'
 require 'icalendar/tzinfo'
+
+require 'new_relic/agent/method_tracer'
+Icalendar::Calendar.class_eval do
+  include ::NewRelic::Agent::MethodTracer
+
+  add_method_tracer :add_event, 'Custom/add_event'
+  add_method_tracer :publish, 'Custom/publish'
+  add_method_tracer :to_ical, 'Custom/to_ical'
+end  
