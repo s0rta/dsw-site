818c306cac42004c87c5c23ba5d5baac73c1a157
Jay Zeschin <jay.zeschin@modeset.com>
2017-10-27 10:08:37 -0600

    Add a batch action to merge duplicated companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,15 @@ class Company < ApplicationRecord
 
   has_many :registrations, dependent: :restrict_with_error
   has_many :submissions, dependent: :restrict_with_error
+
+  def self.combine!(*companies)
+    winner, *rest = companies
+    transaction do
+      rest.each do |loser|
+        loser.submissions.update_all(company_id: winner.id)
+        loser.registrations.update_all(company_id: winner.id)
+        loser.destroy!
+      end
+    end
+  end
 end
