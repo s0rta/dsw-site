b16606e728b8cd223c0f4e07315c967b65ececcb
Ricky Ng <ricky.ng@slalom.com>
2018-06-04 22:27:37 -0600

    removes unneeded test

--- spec/features/managing_venues_spec.rb
+++ spec/features/managing_venues_spec.rb
@@ -7,7 +7,7 @@ feature 'Managing My Venue' do
   end
 
   let(:venue_host_user) do
-    create(:user, email: 'test@example.com', password: 'password', :is_venue_host: true)
+    create(:user, email: 'test@example.com', password: 'password', is_venue_host: true)
   end
 
   before do
@@ -21,11 +21,25 @@ feature 'Managing My Venue' do
     expect(page).to_not have_link('My Venues')
   end
 
-  scenario 'a user who is a venue host should be able to create new venues' do 
-    login_as user, scope: :user
+  scenario 'a user who is a venue host should be able to create new venues' do
+    login_as venue_host_user, scope: :user
 
     visit '/dashboard'
 
     expect(page).to have_link('My Venues')
+    click_on 'My Venues'
+
+    click_on 'New Venue'
+
+    fill_in 'Venue Name', with: 'test venue'
+    fill_in 'Description', with: 'some description'
+    fill_in 'Address', with: '111'
+    fill_in 'City', with: 'Denver'
+    fill_in 'State', with: 'Colorado'
+
+    click_button 'Submit'
+
+    expect(page).to have_content('Venue was successfully created.')
+    expect(page).to have_content('test venue')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -30,8 +30,9 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = 'random'
 
-  # Adds Device Controller Helper for Warden
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.after :each do
+    Warden.test_reset!
+  end
 end
