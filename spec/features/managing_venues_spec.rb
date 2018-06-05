def8db171238bdc8b6e7f9c0c2407a96ff0fe269
Ricky Ng <ricky.ng@slalom.com>
2018-06-04 22:26:33 -0600

    adds feature tests

---
+++ spec/features/managing_venues_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'Managing My Venue' do
+
+  let(:user) do
+    create(:user, email: 'test@example.com', password: 'password')
+  end
+
+  let(:venue_host_user) do
+    create(:user, email: 'test@example.com', password: 'password', :is_venue_host: true)
+  end
+
+  before do
+    allow(ListSubscriptionJob).to receive(:perform_async)
+  end
+
+  scenario 'a user who is not a venue host should not see the \'My Venues\' tab' do
+    login_as user, scope: :user
+    visit '/dashboard'
+
+    expect(page).to_not have_link('My Venues')
+  end
+
+  scenario 'a user who is a venue host should be able to create new venues' do 
+    login_as user, scope: :user
+
+    visit '/dashboard'
+
+    expect(page).to have_link('My Venues')
+  end
+end
