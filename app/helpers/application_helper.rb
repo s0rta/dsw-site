c0290104576b2055f03d8ede270b0dc2277170db
Jay Zeschin <jay.zeschin@modeset.com>
2018-06-14 11:52:54 -0600

    Add podcast sponsorship level
    - Display podcast sponsors on podcast landing page
    - Display podcast sponsors on sponsors page too
    - Tidy up podcast page headers
    - Hound fixes in helpers file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,14 +46,17 @@ module ApplicationHelper
 
   def process_with_pipeline(content)
     context = {
-      asset_root: "/images/icons"
+      asset_root: '/images/icons'
     }
-    pipeline = HTML::Pipeline.new([
-      HTML::Pipeline::MarkdownFilter,
-      HTML::Pipeline::EmojiFilter,
-      HTML::Pipeline::SanitizationFilter,
-      HTML::Pipeline::AutolinkFilter
-    ], context)
+    pipeline = HTML::Pipeline.new(
+      [
+        HTML::Pipeline::MarkdownFilter,
+        HTML::Pipeline::EmojiFilter,
+        HTML::Pipeline::SanitizationFilter,
+        HTML::Pipeline::AutolinkFilter
+      ],
+      context
+    )
     result = pipeline.call content
     result[:output].html_safe
   end
@@ -81,7 +84,7 @@ module ApplicationHelper
     full_hours = hours.floor
     minutes = (hours - full_hours) * 60
     full_minutes = minutes.floor
-    "#{full_days} : #{full_hours.to_s.rjust(2, "0")} : #{full_minutes.to_s.rjust(2, "0")}"
+    "#{full_days} : #{full_hours.to_s.rjust(2, '0')} : #{full_minutes.to_s.rjust(2, '0')}"
   end
 
   def tracks_for_select
@@ -101,10 +104,10 @@ module ApplicationHelper
       { title: 'Mentors & Advisors, "Roster by Specialty - to see list click "Book It"',
         timeslot: 'Friday 9/29: 11am-2pm',
         signup_url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT-sHP3qtTXcm54cnKDd4DKcGPO9iKCGvvYqM_3kZx2JH-K3gqRBzubNaSfSaELEpgyK2U0heh1ApDz/pubhtml?gid=2083275987&single=true' },
-       { title: 'Dave Harris, "Marketing & Sales, Money, Investment, Funding, Planning & Strategy", Rockies Venture Fund',
+      { title: 'Dave Harris, "Marketing & Sales, Money, Investment, Funding, Planning & Strategy", Rockies Venture Fund',
         timeslot: 'Friday 9/29: 11-1pm',
         signup_url: 'http://slottd.com/events/lsz5qsabxv/slots' },
-       { title: 'David Kendall, "Founder, Legal, Organizational Culture", Bold Legal, LLC',
+      { title: 'David Kendall, "Founder, Legal, Organizational Culture", Bold Legal, LLC',
         timeslot: 'Friday 9/29: 11-1pm',
         signup_url: 'http://slottd.com/events/7e752b1i58/slots' },
       { title: 'Nichole Montoya, "Investment, Funding, Planning & Strategy", Cheddar Up',
@@ -127,7 +130,7 @@ module ApplicationHelper
 
   def group_mentor_sessions
     [
-     { title: 'Sameer Dholakia, CEO, SendGrid',
+      { title: 'Sameer Dholakia, CEO, SendGrid',
         timeslot: 'Tuesday 9/26: 10:00am-11:00am',
         signup_url: 'https://www.denverstartupweek.org/schedule/3776-group-mentor-session-with-sameer-dholakia-ceo-of-sendgrid' },
       { title: 'Justin Cucci, Composer & Chef, Edible Beats',
@@ -139,61 +142,69 @@ module ApplicationHelper
       { title: 'Ryan Kirkpatrick, Partner, CO Impact Fund',
         timeslot: 'Wednesday 9/27: 10:00am-11:00am',
         signup_url: 'https://www.denverstartupweek.org/schedule/3778-group-mentor-session-with-ryan-kirkpatrick-partner-at-colorado-impact-fund' },
-       { title: 'Nancy Phillips, Founder & Executive Chair to the Board, ViaWest',
+      { title: 'Nancy Phillips, Founder & Executive Chair to the Board, ViaWest',
         timeslot: 'Thursday 9/28: 3:00pm-4:00pm',
         signup_url: 'https://www.denverstartupweek.org/schedule/3772-group-mentor-session-with-nancy-phillips-president-ceo-of-viawest' },
       { title: 'Linda Appel Lipsius, Co-Founder & CEO, Teatulia Organic Teas & The Mamahood',
         timeslot: 'Friday 9/29: 1:00pm-2:00pm',
         signup_url: 'https://www.denverstartupweek.org/schedule/3819-group-mentor-session-with-linda-appel-lipsius-co-founder-ceo-of-teatulia-organic-teas-the-mama-hood' },
-       { title: 'Nicole Glaros, Chief Innovation Officer, Techstars',
+      { title: 'Nicole Glaros, Chief Innovation Officer, Techstars',
         timeslot: 'Friday 9/29: 11:00am-12:00pm',
-        signup_url: 'https://www.denverstartupweek.org/schedule/3840-group-mentor-session-with-nicole-glaros-chief-innovation-officer-techstars'},
+        signup_url: 'https://www.denverstartupweek.org/schedule/3840-group-mentor-session-with-nicole-glaros-chief-innovation-officer-techstars' }
     ]
   end
 
   def basecamp_sessions
-    Submission.
-      for_current_year.
-      for_schedule.
-      joins(:track).
-      where(tracks: { name: 'Basecamp' })
+    Submission
+      .for_current_year
+      .for_schedule
+      .joins(:track)
+      .where(tracks: { name: 'Basecamp' })
   end
 
   def sponsorships_by_level
-    @_sponsorships_by_level ||= Sponsorship.
-                                for_current_year.
-                                for_sponsors_page.
-                                alphabetical.
-                                includes(:track, submission: :track).
-                                group_by(&:level)
+    @sponsorships_by_level ||= Sponsorship
+                               .for_current_year
+                               .for_sponsors_page
+                               .alphabetical
+                               .includes(:track, submission: :track)
+                               .group_by(&:level)
   end
 
   def ambassador_host_company_sponsorships
-    @_ambassador_host_company_sponsorships ||= Sponsorship.
-                                               for_current_year.
-                                               where(level: Sponsorship::AMBASSADOR_HOST_LEVEL).
-                                               alphabetical.
-                                               includes(:track, submission: :track)
+    @ambassador_host_company_sponsorships ||= Sponsorship
+                                              .for_current_year
+                                              .where(level: Sponsorship::AMBASSADOR_HOST_LEVEL)
+                                              .alphabetical
+                                              .includes(:track, submission: :track)
   end
 
   def ambassador_sponsorships
-    @_ambassador_sponsorships ||= Sponsorship.
-                                  for_current_year.
-                                  where(level: Sponsorship::AMBASSADOR_SPONSOR_LEVEL).
-                                  alphabetical.
-                                  includes(:track, submission: :track)
+    @ambassador_sponsorships ||= Sponsorship
+                                 .for_current_year
+                                 .where(level: Sponsorship::AMBASSADOR_SPONSOR_LEVEL)
+                                 .alphabetical
+                                 .includes(:track, submission: :track)
   end
 
   def ambassador_partners
-    @_ambassador_partners ||= Sponsorship.
-                              for_current_year.
-                              where(level: Sponsorship::AMBASSADOR_PARTNER_LEVEL).
-                              alphabetical.
-                              includes(:track, submission: :track)
+    @ambassador_partners ||= Sponsorship
+                             .for_current_year
+                             .where(level: Sponsorship::AMBASSADOR_PARTNER_LEVEL)
+                             .alphabetical
+                             .includes(:track, submission: :track)
   end
 
   def footer_sponsors
-    @_footer_sponsors ||= Sponsorship.for_current_year.title.alphabetical
+    @footer_sponsors ||= Sponsorship.for_current_year.title.alphabetical
+  end
+
+  def podcast_sponsorships
+    @podcast_sponsorships ||= Sponsorship
+                              .for_current_year
+                              .where(level: Sponsorship::PODCAST_LEVEL)
+                              .alphabetical
+                              .includes(:track, submission: :track)
   end
 
   def helpscout_articles_for_category(category_name)
