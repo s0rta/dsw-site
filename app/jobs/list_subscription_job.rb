dc6d5f8c61bbfd8f8cbe5518c171896754edc192
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2018-08-26 13:53:46 -0600

    Configure ListSubscriptionJob for submitter/presenter lists

--- app/jobs/list_subscription_job.rb
+++ app/jobs/list_subscription_job.rb
@@ -22,14 +22,58 @@ class ListSubscriptionJob
   end
 
   def subscribe_to_sendgrid(email, extra_fields)
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    list_id = ENV['SENDGRID_LIST_ID']
+    recipient_id = create_or_update_recipient(email, extra_fields)
+    list_ids_to_add_to(extra_fields).each do |list_id|
+      response = sg.client.contactdb.lists._(list_id).recipients._(recipient_id).post
+      raise SendGridListAddFailedError.new('Error adding recipient to list!') unless response.status_code.to_s == '201'
+    end
+  end
+
+  private
+
+  def create_or_update_recipient(email, extra_fields)
     data = { email: email }.merge(extra_fields.symbolize_keys.slice(*SENDGRID_FIELDS))
     response = sg.client.contactdb.recipients.patch(request_body: [ data ])
     raise SendGridContactCreateFailedError.new('Error updating recipient!') unless response.status_code.to_s == '201'
-    recipient_id = JSON.parse(response.body)['persisted_recipients'].first
-    response = sg.client.contactdb.lists._(list_id).recipients._(recipient_id).post
-    raise SendGridListAddFailedError.new('Error adding recipient to list!') unless response.status_code.to_s == '201'
+    JSON.parse(response.body)['persisted_recipients'].first
   end
 
+  def list_ids_to_add_to(extra_fields)
+    # Start with the general info list
+    list_ids = [ ENV['SENDGRID_LIST_ID'] ]
+
+    # Create/add submitted lists
+    if submitted_years = extra_fields['submitted_years']
+      submitted_years.each do |year|
+        list_name = "DSW Submitters #{year}"
+        list_ids << all_lists[list_name] || create_list(list_name)
+      end
+    end
+
+    # Create/add confirmed lists
+    if confirmed_years = extra_fields['confirmed_years']
+      confirmed_years.each do |year|
+        list_name = "DSW Presenters #{year}"
+        list_ids << all_lists[list_name] || create_list(list_name)
+      end
+    end
+
+    list_ids
+  end
+
+  def sg
+    @sg ||= SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  end
+
+  def create_list(name)
+    response = sg.client.contactdb.lists.post(request_body: { name: '' })
+    raise SendGridContactCreateFailedError.new("Error creating list #{name}!") unless response.status_code.to_s == '201'
+    response['id']
+  end
+
+  def all_lists
+    @all_lists ||= JSON.parse(sg.client.contactdb.lists.get().body).each_with_object({}) do |list, memo|
+      memo[list['name']] = list['id']
+    end
+  end
 end
