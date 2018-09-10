0361ee0e2221d66b0e491d10abff179b33ceacc7
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2018-09-10 09:38:20 -0600

    Roll out URL validation across  few more models

--- spec/models/sponsorship_spec.rb
+++ spec/models/sponsorship_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Sponsorship, type: :model do
   it { is_expected.to validate_presence_of(:level) }
   it { is_expected.to validate_inclusion_of(:level).in_array(Sponsorship::LEVELS) }
   it { is_expected.to belong_to(:track) }
+  it { is_expected.to allow_value('http://www.google.com/').for(:link_href) }
+  it { is_expected.not_to allow_value('google.com/').for(:link_href) }
 
   it 'defaults its year to the current year' do
     expect(Sponsorship.new.year).to eq(Date.today.year)
