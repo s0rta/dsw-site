6387688e47b0a95e97be177f68fe1848181b7b80
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2018-09-18 23:20:43 -0600

    Safen up default user avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -18,9 +18,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
     if version_name
-      gravatar_url(model.email, version_name.to_sym, default: :mm)
+      gravatar_url(model.try(:email), version_name.to_sym, default: :mm)
     else
-      gravatar_url(model.email, default: :mm)
+      gravatar_url(model.try(:email), default: :mm)
     end
   end
 
