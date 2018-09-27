90a2d4b333f22bf3f3b065bbacfc62e3b6b22639
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2018-09-27 16:49:24 -0600

    Add a simple feedback admin

---
+++ app/admin/feedback.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Feedback do
+
+  menu parent: 'Sessions', priority: 1
+
+  config.sort_order = 'created_at_desc'
+
+  index do
+    selectable_column
+    column :created_at
+    column :submission
+    column :comments
+    column :rating
+    actions
+  end
+
+end
