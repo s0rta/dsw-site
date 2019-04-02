29db21ff45657dbd47c04268bb169fae9276a946
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-04-01 22:37:20 -0600

    Add pink and mint color options for tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,27 +1,26 @@
 class Track < ApplicationRecord
-
   include ValidatedVideoUrl
 
   ICONS = %w[person chart eyeball phone terminal wrench star basecamp martini].freeze
-  COLORS = %w[green teal blue purple gold orange headline-session basecamp-session red].freeze
+  COLORS = %w[green teal blue purple gold orange headline-session basecamp-session red pink mint].freeze
 
   validates :name,
-            :email_alias,
-            :icon,
-            :color, presence: true
+    :email_alias,
+    :icon,
+    :color, presence: true
 
-  validates :color, inclusion: { in: COLORS }
-  validates :icon, inclusion: { in: ICONS }
+  validates :color, inclusion: {in: COLORS}
+  validates :icon, inclusion: {in: ICONS}
 
   has_many :submissions, dependent: :destroy
-  has_and_belongs_to_many :chairs, class_name: 'User'
+  has_and_belongs_to_many :chairs, class_name: "User"
 
   def self.in_display_order
-    order('display_order ASC, name ASC')
+    order("display_order ASC, name ASC")
   end
 
   def self.with_icon_and_color
-    where('icon IS NOT NULL AND color IS NOT NULL')
+    where("icon IS NOT NULL AND color IS NOT NULL")
   end
 
   def self.submittable
@@ -33,6 +32,6 @@ class Track < ApplicationRecord
   end
 
   def name_for_partial
-    name.downcase.tr(' ', '_')
+    name.downcase.tr(" ", "_")
   end
 end
