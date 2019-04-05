8b7c64a3a5553a9479bee970dce51cafac26c28c
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-04-05 12:34:39 -0600

    Reformat spec files

--- spec/features/managing_venues_spec.rb
+++ spec/features/managing_venues_spec.rb
@@ -1,45 +1,44 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature 'Managing My Venue' do
-
+feature "Managing My Venue" do
   let(:user) do
-    create(:user, email: 'test@example.com', password: 'password')
+    create(:user, email: "test@example.com", password: "password")
   end
 
   let(:venue_host_user) do
-    create(:user, email: 'test@example.com', password: 'password')
+    create(:user, email: "test@example.com", password: "password")
   end
 
   before do
     allow(ListSubscriptionJob).to receive(:perform_async)
   end
 
-  scenario 'a user who is not a venue host should not see the \'My Venues\' tab' do
+  scenario "a user who is not a venue host should not see the 'My Venues' tab" do
     login_as user, scope: :user
-    visit '/dashboard'
+    visit "/dashboard"
 
-    expect(page).to_not have_link('My Venues')
+    expect(page).to_not have_link("My Venues")
   end
 
-  scenario 'a user who is a venue host should be able to create new venues' do
+  scenario "a user who is a venue host should be able to create new venues" do
     login_as venue_host_user, scope: :user
 
-    visit '/dashboard'
+    visit "/dashboard"
 
-    expect(page).to have_link('My Venues')
-    click_on 'My Venues'
+    expect(page).to have_link("My Venues")
+    click_on "My Venues"
 
-    click_on 'New Venue'
+    click_on "New Venue"
 
-    fill_in 'Venue Name', with: 'test venue'
-    fill_in 'Description', with: 'some description'
-    fill_in 'Address', with: '111'
-    fill_in 'City', with: 'Denver'
-    fill_in 'State', with: 'Colorado'
+    fill_in "Venue Name", with: "test venue"
+    fill_in "Description", with: "some description"
+    fill_in "Address", with: "111"
+    fill_in "City", with: "Denver"
+    fill_in "State", with: "Colorado"
 
-    click_button 'Submit'
+    click_button "Submit"
 
-    expect(page).to have_content('Venue was successfully created.')
-    expect(page).to have_content('test venue')
+    expect(page).to have_content("Venue was successfully created.")
+    expect(page).to have_content("test venue")
   end
 end
