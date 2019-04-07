d081a1991376b78b9095c3d980b576796d0efe61
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-04-07 15:30:01 -0600

    Adjust company display column in venues admin

--- app/admin/venues.rb
+++ app/admin/venues.rb
@@ -1,21 +1,20 @@
 ActiveAdmin.register Venue do
-
-  menu parent: 'Sessions'
+  menu parent: "Sessions"
 
   permit_params :contact_email,
-                :contact_name,
-                :contact_phone,
-                :description,
-                :name,
-                :address,
-                :suite_or_unit,
-                :city,
-                :state,
-                :seated_capacity,
-                :standing_capacity,
-                :av_capabilities,
-                :extra_directions,
-                :company_id
+    :contact_name,
+    :contact_phone,
+    :description,
+    :name,
+    :address,
+    :suite_or_unit,
+    :city,
+    :state,
+    :seated_capacity,
+    :standing_capacity,
+    :av_capabilities,
+    :extra_directions,
+    :company_id
 
   index do
     selectable_column
@@ -24,7 +23,7 @@ ActiveAdmin.register Venue do
     column :contact_email
     column :seated_capacity
     column :standing_capacity
-    column :company_id
+    column :company
     actions
   end
 
@@ -37,7 +36,7 @@ ActiveAdmin.register Venue do
   form do |f|
     f.inputs do
       f.input :name
-      f.input :description, hint: 'This will not be displayed publicly'
+      f.input :description, hint: "This will not be displayed publicly"
       f.input :address
       f.input :suite_or_unit
       f.input :city
@@ -45,11 +44,17 @@ ActiveAdmin.register Venue do
       f.input :seated_capacity
       f.input :standing_capacity
       f.input :av_capabilities
-      f.input :extra_directions, hint: 'These will be displayed to the public next to the address'
+      f.input :extra_directions, hint: "These will be displayed to the public next to the address"
       f.input :contact_name
-      f.input :contact_email, hint: 'Multiple addresses are allowed; separate them with commas'
+      f.input :contact_email, hint: "Multiple addresses are allowed; separate them with commas"
       f.input :contact_phone
-      f.input :company_id, as: :ajax_select, data: { url: filter_admin_companies_path, search_fields: [:name], ajax_search_fields: [:company_id] }
+      f.input :company_id,
+        as: :ajax_select,
+        data: {
+          url: filter_admin_companies_path,
+          search_fields: [:name],
+          ajax_search_fields: [:company_id],
+        }
     end
     f.actions
   end
@@ -72,15 +77,14 @@ ActiveAdmin.register Venue do
       end
 
       tab :schedule do
-        panel 'Schedule' do
+        panel "Schedule" do
           table_for venue_availabilities.order(day: :asc, time_block: :asc) do
-            column 'Year', sortable: true, &:year
-            column 'Date Time', sortable: true, &:human_date_time
-            column 'Title', &:submission
+            column "Year", sortable: true, &:year
+            column "Date Time", sortable: true, &:human_date_time
+            column "Title", &:submission
           end
         end
       end
     end
   end
-
 end
