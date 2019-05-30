1d29095da3fe9c470e87d235bc9684d9a7997806
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-05-30 14:17:48 -0500

    Turn on venue admin before tests

--- spec/features/managing_venues_spec.rb
+++ spec/features/managing_venues_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 feature "Managing My Venue" do
-  before do
-    Flipper[:venue_admin].enable
-  end
-
   let(:user) do
     create(:user, email: "test@example.com", password: "password")
   end
@@ -17,11 +13,12 @@ feature "Managing My Venue" do
     end
   end
 
-  before do
+  before(:each) do
     allow(ListSubscriptionJob).to receive(:perform_async)
   end
 
   scenario "a user who is not a venue host should not see the 'My Venues' tab" do
+    Flipper[:venue_admin].disable
     login_as user, scope: :user
     visit "/dashboard"
 
@@ -29,6 +26,7 @@ feature "Managing My Venue" do
   end
 
   scenario "a user who is a venue host should be able to create new venues" do
+    Flipper[:venue_admin].enable
     login_as venue_host_user, scope: :user
 
     visit "/dashboard"
