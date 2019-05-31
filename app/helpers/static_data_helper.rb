55cd3e23f0c1843dc273edec384bb69592ba3b1c
Seth Paye <spaye@skookum.com>
2019-05-31 11:05:53 -0400

    Make a static data helper

---
+++ app/helpers/static_data_helper.rb
@@ -0,0 +1,16 @@
+module StaticDataHelper
+  def featured_static_data
+    HashWithIndifferentAccess.new(
+      YAML.safe_load(File.read(File.expand_path("../data/featured.yml",  __dir__))))
+  end
+
+  def secondary_nav_static_data
+    HashWithIndifferentAccess.new(
+      YAML.safe_load(File.read(File.expand_path("../data/secondary_nav.yml",  __dir__))))
+  end
+
+  def programs_static_data
+    HashWithIndifferentAccess.new(
+      YAML.safe_load(File.read(File.expand_path("../data/programs.yml",  __dir__))))
+  end
+end
