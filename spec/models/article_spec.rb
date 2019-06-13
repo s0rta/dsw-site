c449b7e64056db56cfbb24fc95520748c107e8c3
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-06-13 12:05:14 -0600

    One more transposition change

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  it { is_expected.to validate_presence_of(:title).is_at_most(150) }
+  it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_length_of(:title).is_at_most(150) }
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to belong_to(:author) }
