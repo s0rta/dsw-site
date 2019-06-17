946f94f39839c808fd7584e5db11e78f2adc95ad
Seth Paye <spaye@skookum.com>
2019-06-17 16:08:33 -0400

    whoops

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,10 +7,10 @@ class DashboardsController < ApplicationController
     @articles = current_user.articles
     @previous_submissions = current_user.submissions.for_previous_years.order('submissions.created_at DESC')
     @my_schedule = Submission
-                  .for_year(Date.today.year).
-                  .for_schedule.
-                  .my_schedule(current_user).
-                  .order(:start_hour).
+                  .for_year(Date.today.year)
+                  .for_schedule
+                  .my_schedule(current_user)
+                  .order(:start_hour)
                   .includes(:venue,
                     :submitter,
                     :track,
