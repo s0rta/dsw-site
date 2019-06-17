b37cfb32fed23686b3e320fe0a5f52e44da741d8
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-06-17 09:51:51 -0600

    Linter fixes

--- spec/features/editing_profile_spec.rb
+++ spec/features/editing_profile_spec.rb
@@ -1,28 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature 'Editing my profile' do
-
+feature "Editing my profile" do
   before do
     allow(ListSubscriptionJob).to receive(:perform_async)
   end
 
-  scenario 'Creating an editing an account from the dashboard' do
-    visit '/dashboard'
-    click_link 'Create an account'
-    fill_in 'Name', with: 'Test Registrant'
-    fill_in 'E-mail Address', with: 'test2@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Confirm Password', with: 'password'
-    click_button 'Sign Up'
-    click_link 'My Profile'
-    attach_file('user_avatar', Rails.root.join('spec', 'support', 'avatar_examples', 'more_than_2_mb.jpg'))
+  scenario "Creating an editing an account from the dashboard" do
+    visit "/dashboard"
+    click_link "Create an account"
+    fill_in "Name", with: "Test Registrant"
+    fill_in "E-mail Address", with: "test2@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm Password", with: "password"
+    click_button "Sign Up"
+    click_link "My Profile"
+    attach_file("user_avatar", Rails.root.join("spec", "support", "avatar_examples", "more_than_2_mb.jpg"))
     page.driver.browser.switch_to.alert.accept
-    attach_file('user_avatar', Rails.root.join('spec', 'support', 'avatar_examples', 'less_than_2_mb.png'))
-    fill_in 'Name', with: 'Test Registrant 2'
-    fill_in 'Tagline', with: 'Founder at Pied Piper'
-    fill_in 'user_current_password', with: 'password'
-    click_button 'Update'
-    expect(page).to have_field('Name', with: 'Test Registrant 2')
-    expect(page).to have_field('Tagline', with: 'Founder at Pied Piper')
+    attach_file("user_avatar", Rails.root.join("spec", "support", "avatar_examples", "less_than_2_mb.png"))
+    fill_in "Name", with: "Test Registrant 2"
+    fill_in "Tagline", with: "Founder at Pied Piper"
+    fill_in "user_current_password", with: "password"
+    click_button "Update"
+    expect(page).to have_field("Name", with: "Test Registrant 2")
+    expect(page).to have_field("Tagline", with: "Founder at Pied Piper")
   end
 end
--- spec/uploaders/avatar_spec.rb
+++ spec/uploaders/avatar_spec.rb
@@ -1,15 +1,15 @@
-require 'carrierwave/test/matchers'
-require 'spec_helper'
+require "carrierwave/test/matchers"
+require "spec_helper"
 
 describe AvatarUploader do
   include CarrierWave::Test::Matchers
 
-  let(:user) { double('user') }
+  let(:user) { double("user") }
   let(:uploader) { AvatarUploader.new(user, :avatar) }
 
   before do
     AvatarUploader.enable_processing = true
-    File.open(Rails.root.join('spec', 'support', 'avatar_examples', 'less_than_2_mb.png')) { |f| uploader.store!(f) }
+    File.open(Rails.root.join("spec", "support", "avatar_examples", "less_than_2_mb.png")) { |f| uploader.store!(f) }
   end
 
   after do
@@ -17,13 +17,13 @@ describe AvatarUploader do
     uploader.remove!
   end
 
-  context 'the thumb version' do
-    it 'scales down a landscape image to be exactly 100 by 100 pixels' do
+  context "the thumb version" do
+    it "scales down a landscape image to be exactly 100 by 100 pixels" do
       expect(uploader.thumb).to have_dimensions(100, 100)
     end
   end
 
-  it 'has the correct format' do
-    expect(uploader).to be_format('png')
+  it "has the correct format" do
+    expect(uploader).to be_format("png")
   end
 end
