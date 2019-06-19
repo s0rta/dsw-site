f63fa0c94959a24e3520566ab3aae12152125c47
Seth Paye <spaye@skookum.com>
2019-06-19 14:42:27 -0400

    Add ability to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
+    @article.update(article_params)
+    if @article.save
+      flash[:notice] = "Thanks! Your article update has been received!"
+      redirect_to dashboard_path
+    else
+      respond_with @article
+    end
   end
 
   def article_params
