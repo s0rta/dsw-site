606004060c8142e96e69e444824f97c4e89aada1
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-06-30 13:49:28 -0600

    Ensure that we raise on unpermitted params
    Apparently the way I was guaranteeing this previously doesn’t work.

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,8 +23,8 @@ module Denverstartupweek
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.eager_load_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W[#{config.root}/lib]
+    config.eager_load_paths += %W[#{config.root}/lib]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -35,7 +35,7 @@ module Denverstartupweek
     # config.i18n.default_locale = :de
 
     # Set a default host for mailers
-    config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_HOST'] }
+    config.action_mailer.default_url_options = {host: ENV["ACTION_MAILER_HOST"]}
 
     # Use a SQL schema format to allow fulltext indices to be dumped
     config.active_record.schema_format = :sql
@@ -43,5 +43,8 @@ module Denverstartupweek
     config.action_view.field_error_proc = proc { |html_tag, _instance|
       html_tag
     }
+
+    # Raise on unpermitted parameters so we find out about them early
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
