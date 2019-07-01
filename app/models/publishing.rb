bf1250ed625788d80762b565f367dd3f8340701f
Seth Paye <spaye@skookum.com>
2019-07-01 11:27:11 -0400

    Lint fix

--- app/models/publishing.rb
+++ app/models/publishing.rb
@@ -3,8 +3,12 @@ class Publishing < ApplicationRecord
 
   def self.filtered_results(filters)
     articles = Article.for_publishings_filter(filters)
-    sessions = AnnualSchedule.registration_open? || AnnualSchedule.post_week? ? Submission.for_publishings_filter(filters) : []
+    sessions = []
+    if AnnualSchedule.registration_open? || AnnualSchedule.post_week?
+      sessions = Submission.for_publishings_filter(filters)
+    end
     ids = articles.pluck(:id) + sessions.pluck(:id)
+
     where(subject_id: ids)
     .order(effective_at: :desc)
   end
