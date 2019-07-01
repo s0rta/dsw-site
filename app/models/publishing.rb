3bd8d65f8aaf55e5b624f6d0af341c0e295fc3ef
Seth Paye <spaye@skookum.com>
2019-07-01 10:44:44 -0400

    Add filtering to Publishing model

--- app/models/publishing.rb
+++ app/models/publishing.rb
@@ -1,3 +1,13 @@
 class Publishing < ApplicationRecord
   belongs_to :subject, polymorphic: true
+
+  def self.filtered_results(filters)
+    articles = Article.for_publishings_filter(filters)
+    sessions = AnnualSchedule.registration_open? || AnnualSchedule.post_week? ?
+      Submission.for_publishings_filter(filters) : []
+    ids = articles.pluck(:id) + sessions.pluck(:id)
+    where(subject_id: ids)
+    .order(effective_at: :desc)
+  end
+
 end
