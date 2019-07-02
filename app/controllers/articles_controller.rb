d2c24742eb6a0d4fc176c1ccb0d269ebef3ae1e5
Seth Paye <spaye@skookum.com>
2019-07-02 11:15:35 -0400

    24 per page, not 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: %i[new create update edit]
 
   def index
-    @publishings = Publishing.filtered_results(params).page(params[:page]).per(1)
+    @publishings = Publishing.filtered_results(params).page(params[:page]).per(24)
 
     respond_to do |format|
       format.html
