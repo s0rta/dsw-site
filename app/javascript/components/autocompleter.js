ec82ca8b72749b115448f9df4b4c12cad546114e
Seth Paye <spaye@skookum.com>
2019-07-02 12:51:02 -0400

    Update auto completer to be able to add values to hidden input fields

--- app/javascript/components/autocompleter.js
+++ app/javascript/components/autocompleter.js
@@ -1,20 +1,32 @@
-import Bindable from 'utensils/bindable'
-import Awesomplete from 'awesomplete'
+import Bindable from 'utensils/bindable';
+import Awesomplete from 'awesomplete';
 
 export default class Autocompleter {
   constructor(el, data) {
     this.el = el;
     this.data = data ? data : this.el.data();
+    this.replaceOverride = this.replaceOverride.bind(this);
     this.awesomplete = new Awesomplete(this.el[0], {
       minChars: 1,
       autoFirst: true
     });
-    this.addListeners()
+
+    if (this.data.valuefield) {
+      this.awesomplete.replace = this.replaceOverride;
+    }
+
+    this.addListeners();
+  }
+
+  replaceOverride(suggestion) {
+    this.awesomplete.input.value = suggestion.label;
+    const valueInput = document.getElementById(this.data.valuefield);
+    valueInput.value = suggestion.value;
   }
 
   dispose() {
-    this.removeListeners()
-    this.awesomplete.destroy()
+    this.removeListeners();
+    this.awesomplete.destroy();
   }
 
   addListeners() {
@@ -24,14 +36,14 @@ export default class Autocompleter {
         data: { term: this.value },
         type: 'GET',
         dataType: 'json'
-      }).done((data) => {
+      }).done(data => {
         this.awesomplete.list = data.results;
       });
     });
   }
 
   removeListeners() {
-    this.el.off('keyup')
+    this.el.off('keyup');
   }
 }
 
