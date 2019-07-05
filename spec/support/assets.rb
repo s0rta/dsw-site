efb97b1432a6382cbbc1f9dd66f64cd38f638d1e
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-07-05 17:26:14 -0600

    Move some test logging output to STDERR

--- spec/support/assets.rb
+++ spec/support/assets.rb
@@ -2,15 +2,15 @@ RSpec.configure do |config|
   config.before(:suite) do
     # Precompile assets so our first test doesn't time out
     # http://code.dblock.org/2013/08/04/precompiling-rails-assets-before-rspeccapybara-integration-tests.html
-    message = 'Precompiling assets to preempt Capybara timeout issues'
-    webpacker_ms = Benchmark.ms do
+    message = "Precompiling assets to preempt Capybara timeout issues"
+    webpacker_ms = Benchmark.ms {
       Webpacker.compile
-    end
-    puts 'Webpacker: %s (%.1fms)' % [ message, webpacker_ms ]
-    ms = Benchmark.ms do
+    }
+    warn "Webpacker: %s (%.1fms)" % [message, webpacker_ms]
+    ms = Benchmark.ms {
       assets = Rails.application.config.assets.precompile
       ActionView::Base.assets_manifest.compile(assets)
-    end
-    puts 'Pipeline: %s (%.1fms)' % [ message, ms ]
+    }
+    warn "Pipeline: %s (%.1fms)" % [message, ms]
   end
 end
