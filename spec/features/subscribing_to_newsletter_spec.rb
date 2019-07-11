27f7c3e3b4203efefd3d08700e5e61f097772807
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-07-10 22:58:30 -0600

    Get newsletter subscription UI spec working again

--- spec/features/subscribing_to_newsletter_spec.rb
+++ spec/features/subscribing_to_newsletter_spec.rb
@@ -1,12 +1,12 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature 'Subscribing to the newsletter' do
-  scenario 'from the page footer' do
-    pending('refactor')
-    visit '/'
-    fill_in 'Email Address', with: 'test@example.com'
+feature "Subscribing to the newsletter" do
+  scenario "from the page footer" do
+    visit "/"
+    fill_in "Email Address", with: "test@example.com"
     page.execute_script("document.querySelector('input[type=\"submit\"]').scrollIntoView(false)")
-    click_button 'Subscribe to Updates'
-    expect(page).to have_button('Thanks! You are signed up for updates.', disabled: true)
+    click_button "Get Updates"
+    expect(page).to have_button("Thanks! You are signed up for updates.", disabled: true)
+    expect(NewsletterSignup.last.email).to eq("test@example.com")
   end
 end
