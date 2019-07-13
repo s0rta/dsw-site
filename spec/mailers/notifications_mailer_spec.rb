b181c2983d083f6494211e723e8e67ad3968dc34
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-07-13 15:03:50 -0600

    Change transaction email “from” header
    Move it to the white labeled email.denverstartupweek.org subdomain
    
    Fixes #553

--- spec/mailers/notifications_mailer_spec.rb
+++ spec/mailers/notifications_mailer_spec.rb
@@ -1,16 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe NotificationsMailer, type: :mailer do
-
   let(:session) { build(:submission) }
 
-  describe 'the thanks e-mail' do
+  describe "the thanks e-mail" do
     let(:mail) { described_class.session_thanks(session) }
 
-    it 'renders properly' do
-      expect(mail).to deliver_from('Denver Startup Week <info@denverstartupweek.org>')
+    it "renders properly" do
+      expect(mail).to deliver_from("Denver Startup Week <info@email.denverstartupweek.org>")
       expect(mail).to deliver_to(session.submitter.email, session.contact_email)
-      expect(mail).to have_subject('Thank you, DSW session organizers!')
+      expect(mail).to have_subject("Thank you, DSW session organizers!")
     end
   end
 end
