340c326020d02c99674d4fbe699845bfa88cf3d8
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-07-29 21:31:39 -0600

    Set up Recaptcha for account creation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :check_captcha, only: [:create]
+
   protected
 
   def after_sign_up_path_for(resource)
@@ -7,5 +9,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(resource)
     dashboard_path
+  end
+
+  private
+
+  # Per https://github.com/plataformatec/devise/wiki/How-To:-Use-Recaptcha-with-Devise
+  def check_captcha
+    unless verify_recaptcha(action: "registration")
+      self.resource = resource_class.new sign_up_params
+      resource.validate # Look for any other validation errors besides Recaptcha
+      set_minimum_password_length
+      respond_with resource
+    end
   end
 end
