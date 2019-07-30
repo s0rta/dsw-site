bd97ed80750b5e2696a20ba88e25c44a87039a03
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-07-30 13:06:37 -0600

    Process cluster & ambassador images in background

--- app/models/ambassador.rb
+++ app/models/ambassador.rb
@@ -1,13 +1,13 @@
 class Ambassador < ApplicationRecord
-
   include YearScoped
 
   validates :first_name,
-            :last_name,
-            :company,
-            :title,
-            :location,
-            presence: true
+    :last_name,
+    :company,
+    :title,
+    :location,
+    presence: true
 
   mount_uploader :avatar, AvatarUploader
+  process_in_background :avatar
 end
--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -3,6 +3,7 @@ class Cluster < ApplicationRecord
   has_many :submissions, dependent: :nullify
 
   mount_uploader :header_image, HeaderImageUploader
+  process_in_background :header_image
 
   def self.in_display_order
     order("name DESC")
@@ -13,14 +14,14 @@ class Cluster < ApplicationRecord
   end
 
   def self.template_list_data
-      in_display_order
+    in_display_order
       .active
       .map do |c|
       c.attributes.symbolize_keys.slice(:name, :description, :is_active, :header_image).merge(
         title: c.name,
         header_image_url: c.header_image.url(:content_card)
       )
-      end
+    end
   end
 
   def self.dropdown_options
