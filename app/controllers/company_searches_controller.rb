4f5823254f8ba343831dc25c77b8be38c29163b1
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-08-03 13:46:41 -0600

    Set up company search queries to use fuzzy search
    Instead of ILIKE. Should be dramatically faster.

--- app/controllers/company_searches_controller.rb
+++ app/controllers/company_searches_controller.rb
@@ -1,11 +1,11 @@
 class CompanySearchesController < ApplicationController
   def show
-    @companies = Company.where("name ILIKE ?", "%#{params[:term]}%")
+    @companies = Company.fuzzy_search(name: params[:term])
     render json: {results: @companies.pluck(:name)}.to_json
   end
 
   def mine
-    @companies = current_user.companies.where("name ILIKE ?", "%#{params[:term]}%")
+    @companies = current_user.companies.fuzzy_search(name: params[:term])
     render json: {results: @companies.pluck(:name)}.to_json
   end
 end
