3f1e750b1110917651fac39e23ffcb85dc954a9b
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-08-04 14:59:25 -0600

    Tune down company fuzzy match limit

--- app/controllers/company_searches_controller.rb
+++ app/controllers/company_searches_controller.rb
@@ -1,4 +1,6 @@
 class CompanySearchesController < ApplicationController
+  before_action :set_minimum_match_threshold
+
   def show
     @companies = Company.fuzzy_search(name: params[:term])
     render json: {results: @companies.map(&:name)}.to_json
@@ -7,5 +9,11 @@ class CompanySearchesController < ApplicationController
   def mine
     @companies = current_user.companies.fuzzy_search(name: params[:term])
     render json: {results: @companies.map(&:name)}.to_json
+  end
+
+  private
+
+  def set_minimum_match_threshold
+    ActiveRecord::Base.connection.execute("SELECT set_limit(0.1);")
   end
 end
