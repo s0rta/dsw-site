cb5fa479e954963c08f93b039b888edb81d46656
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-08-06 21:00:34 -0600

    Only send update nags for this year's submissions

--- app/jobs/send_update_reminders_job.rb
+++ app/jobs/send_update_reminders_job.rb
@@ -2,7 +2,7 @@ class SendUpdateRemindersJob
   include Sidekiq::Worker
 
   def perform
-    Submission.where.not(proposed_updates: nil).each do |s|
+    Submission.for_current_year.where.not(proposed_updates: nil).each do |s|
       s.notify_track_chairs_of_update!(true)
     end
   end
