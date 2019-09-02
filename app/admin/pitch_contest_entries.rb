be53e5cce751cde19921684d50c4fe3269dfa2f1
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-09-02 13:58:15 -0600

    Allow year param on pitch contest entries

--- app/admin/pitch_contest_entries.rb
+++ app/admin/pitch_contest_entries.rb
@@ -1,8 +1,8 @@
 ActiveAdmin.register PitchContest::Entry do
-
   permit_params :name,
-                :video_url,
-                :is_active
+    :year,
+    :video_url,
+    :is_active
 
   index do
     selectable_column
@@ -17,8 +17,8 @@ ActiveAdmin.register PitchContest::Entry do
   filter :year
 
   # Set a default year filter
-  scope('Current', default: true, &:for_current_year)
-  scope('Previous Years', &:for_previous_years)
+  scope("Current", default: true, &:for_current_year)
+  scope("Previous Years", &:for_previous_years)
 
   controller do
     def scoped_collection
